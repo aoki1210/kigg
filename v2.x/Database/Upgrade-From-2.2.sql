c8752a4d13685a002455567ee4f4816291261b72
mosessaur <mosessaur@localhost>
2009-05-19 22:11:46 +0000

    Changeset Highlights: Modified sql scripts: *Create.Sql to include new UDF functions EFStorySearch & EFCommentSearch to support Full-Text Search *Modified Data.sql to add 'Unspecified' tag and category *Added Upgrade-From-2.2.sql to include new UDF functions mentioned above

---
+++ v2.x/Database/Upgrade-From-2.2.sql
@@ -0,0 +1,42 @@
+CREATE function [dbo].[EFCommentSearch]
+      (
+		@storyId uniqueidentifier,
+		@query nvarchar(4000)
+      )
+returns bit
+as
+BEGIN
+			if (EXISTS(select [StoryId]
+					   from containstable(StoryComment,(TextBody), @query) as ft
+					   join [StoryComment] on [StoryComment].[ID] = ft.[KEY]
+					   where [StoryComment].[StoryId] = @storyId))
+			BEGIN
+				return 1
+			END			
+				
+				return 0
+			
+		 
+END
+GO
+
+CREATE function [dbo].[EFStorySearch]
+      (
+		@storyId uniqueidentifier,
+		@query nvarchar(4000)
+      )
+returns bit
+as
+BEGIN
+			if (EXISTS(select [Id]
+						from containstable(Story,(Title, TextDescription), @query) as ft
+						join [Story] on [Story].[ID] = ft.[KEY]
+						where [Story].[ID] = @storyId))
+			BEGIN
+				return 1
+			END			
+				return 0
+			
+		 
+END
+GO
