6a10195da60d69859ef7230fc86f10624f699dd9
Moses <Moses@localhost>
2011-11-06 18:46:11 +0800

    Committing missing file kigg.config & CategoryController.cs

---
+++ Source/WebCore/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿namespace Kigg.Web.Controllers
+{
+    using System.Web.Mvc;
+    using Services;
+
+    public class CategoryController : KiggControllerBase
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            Check.Argument.IsNotNull(categoryService, "categoryService");
+
+            this.categoryService = categoryService;
+        }
+
+        [ValidateInput(false)]
+        [ChildActionOnly]
+        public ActionResult Menu()
+        {
+            var categories = categoryService.GetCategoryList();
+            return View("_menu",categories);
+        }
+    }
+}
